976bbeac62bcf2f12a5e5407335562cfb871fa6e
Toan Duong <toandt@nexlesoft.com>
2023-08-09 22:50:44 +0700

    da luu dc flash

--- esp_ap_mode.ino
+++ esp_ap_mode.ino
@@ -1,8 +1,9 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <EEPROM.h>
 
 // Set the credentials for your access point
-const char* ssid = "Free-wifi/12345678";
+const char* ssid = "ig: thank_toanf";
 const char* password = "12345678";
 
 // Create an instance of the web server
@@ -20,6 +21,14 @@ const char* htmlPage = "<html><body>"
                        "</form>"
                        "</body></html>";
 
+// EEPROM addresses for username and password
+int usernameAddr = 0;
+int passwordAddr = 50;
+
+// Timer variables
+unsigned long previousMillis = 0;
+const unsigned long interval = 1000;  // 1 second interval
+
 void setup() {
   // Start serial communication
   Serial.begin(115200);
@@ -32,6 +41,17 @@ void setup() {
   Serial.print("Access Point IP address: ");
   Serial.println(WiFi.softAPIP());
 
+  // Initialize EEPROM
+  EEPROM.begin(512);
+
+  // Read username and password from EEPROM
+  String savedUsername = "";
+  String savedPassword = "";
+  for (int i = 0; i < 50; ++i) {
+    savedUsername += char(EEPROM.read(usernameAddr + i));
+    savedPassword += char(EEPROM.read(passwordAddr + i));
+  }
+
   // Handle root URL request
   server.on("/", []() {
     server.send(200, "text/html", htmlPage);
@@ -41,6 +61,15 @@ void setup() {
   server.on("/login", []() {
     String username = server.arg("username");
     String password = server.arg("password");
+
+    // Save the username and password in EEPROM
+    for (int i = 0; i < username.length(); ++i) {
+      EEPROM.write(usernameAddr + i, username[i]);
+    }
+    for (int i = 0; i < password.length(); ++i) {
+      EEPROM.write(passwordAddr + i, password[i]);
+    }
+    EEPROM.commit();
 
     // Perform your login verification here
     // For demonstration purposes, let's just print the entered credentials
@@ -66,4 +95,21 @@ void setup() {
 void loop() {
   // Handle client requests
   server.handleClient();
+
+  // Check if it's time to print EEPROM contents
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    // Print the EEPROM contents
+    Serial.println("EEPROM Contents:");
+    for (int i = 0; i < 50; ++i) {
+      Serial.print(char(EEPROM.read(usernameAddr + i)));
+    }
+    Serial.println();
+    for (int i = 0; i < 50; ++i) {
+      Serial.print(char(EEPROM.read(passwordAddr + i)));
+    }
+    Serial.println();
+  }
 }
