8e33cca7564b2a743ec0bdbb295b1df30e018bd5
Toan Duong <toandt@nexlesoft.com>
2023-08-09 22:35:19 +0700

    two

--- esp_ap_mode.ino
+++ esp_ap_mode.ino
@@ -5,9 +5,21 @@ const char *ssid = "MyESP8266AP";
 const char *password = "testpassword";
  
 ESP8266WebServer server(80);
- 
+
+// HTML login page
+const char* htmlPage = "<html><body>"
+                       "<h1>Login</h1>"
+                       "<form action='/login' method='POST'>"
+                       "<label for='username'>Username:</label>"
+                       "<input type='text' id='username' name='username'><br><br>"
+                       "<label for='password'>Password:</label>"
+                       "<input type='password' id='password' name='password'><br><br>"
+                       "<input type='submit' value='Submit'>"
+                       "</form>"
+                       "</body></html>";
+                       
 void handleRoot() {
-  server.send(200, "text/html", "<h1>Hello from ESP8266 AP!</h1>");
+  server.send(200, "text/html", htmlPage);
 }
  
 void setup() {
